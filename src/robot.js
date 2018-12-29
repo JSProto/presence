61d128f22feebc882d4c4e3eec78000fd8aef7ea
Jake <jake.arefyev@gmail.com>
2018-12-29 19:45:57 +0200

    original

---
+++ src/robot.js
@@ -0,0 +1,39 @@
+// @flow
+
+const robot = require('robotjs');
+
+robot.setMouseDelay(2);
+
+const delay = (ms = 1000) => new Promise(resolve => setTimeout(resolve, ms));
+
+const job = ms =>
+  async function _(fn) {
+    await fn();
+    await delay(ms);
+    _(fn);
+  };
+
+const run = job(55 * 1000);
+run(() => robot.mouseClick());
+
+// class Mouse {
+//     static async click(){
+//         robot.mouseClick();
+//     }
+//     static async move(){
+
+//         const {x, y} = robot.getMousePos();
+//         const radius = 30; // радиус окружности
+
+//         robot.mouseClick();
+
+//         for (let angle = 0; angle < 6; angle += 0.1) {
+//             let vx = Math.cos(angle) * radius;
+//             let vy = Math.sin(angle) * radius;
+
+//             robot.moveMouse(x + vx, y + vy);
+
+//             await delay(5);
+//         }
+//     }
+// }
